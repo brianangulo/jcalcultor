f00d7662cbbc2fad5c43c78fb07e22fe145d178d
Brian Angulo <brianangulo96@gmail.com>
2021-10-30 15:08:47 -0500

    feat: created grid manager

---
+++ src/GridManager.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class GridManager extends GridLayout {
+   public GridManager() {
+       super(4, 4);
+   }
+   public GridManager(int rows, int cols) {
+       super(rows, cols);
+   }
+}
