dd98f155c38aae3ad4de3571d70e24b74d362b7b
Brian Angulo <brianangulo96@gmail.com>
2021-10-30 15:08:32 -0500

    update: alternate constructor for frame

--- src/Frame.java
+++ src/Frame.java
@@ -2,9 +2,14 @@ import javax.swing.JFrame;
 
 public class Frame extends JFrame {
    public Frame(String title) {
-        super();
         setTitle(title);
         setVisible(true);
         setSize(400, 600);
+    }
+    public Frame() {
+        setTitle("JCalc");
+        setVisible(true);
+        setSize(400, 600);
+        pack();
     }
 }
