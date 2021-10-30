2dc1c52ebf29a7c1a9759c7b0d4d0b6968615643
Brian Angulo <brianangulo96@gmail.com>
2021-10-30 15:08:00 -0500

    feat: button data provider

---
+++ src/ButtonsData.java
@@ -0,0 +1,7 @@
+public class ButtonsData {
+    private static String[] listOfTittles = {"C", "+", "-", "=", "*", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+    public static String[] getButtonTittles() {
+        return listOfTittles;
+    }
+}
