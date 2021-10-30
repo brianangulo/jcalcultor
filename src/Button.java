80c758df5eb37343617018a2e56c3badb413f1ea
Brian Angulo <brianangulo96@gmail.com>
2021-10-30 15:07:47 -0500

    refactor: optimizing Button class

--- src/Button.java
+++ src/Button.java
@@ -2,8 +2,10 @@ import javax.swing.JButton;
 
 public class Button extends JButton {
    public Button(String btnTxt, int buttonWidth, int buttonHeight) {
-        super();
         setText(btnTxt);
         setSize(buttonWidth, buttonHeight);
+    }
+    public Button(String btnTxt) {
+        setText(btnTxt);
     }
 }
